eea3756aa407430586ce7fdb2debb1c0c2e9b96e
Florin Radu <marchidan.florin24@gmail.com>
2025-10-30 15:11:58 +0200

    004 - Create first APIS products and products_category

---
+++ 02_backend/eCommerceApp/src/main/java/com/florinCode/eCommerceApp/dao/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.florinCode.eCommerceApp.dao;
+
+import com.florinCode.eCommerceApp.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(collectionResourceRel = "product", path = "products")
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
